d7bdc0fcccad95564c4ce53f4a5a66acf7bb863e
Janvi Yadav <janvi1552005@gmail.com>
2026-07-21 11:06:37 +0530

    Update oop.py

--- oop.py
+++ oop.py
@@ -224,3 +224,33 @@ class Bank:
         print(si)
 
 si1=Bank.simple_interest(20000,2)
+
+#POLYMORPHISM = same operator is allowed to have different meaning acc to context
+#operator overloading
+#one operator behaving diff 
+print(1+2) #add
+print("Janvi"+"Yadav") #concatenate
+print([1,2,3]+[4,5,6]) #merge
+
+class Complex:
+    def __init__(self,real,img):
+        self.real=real
+        self.img=img
+
+    def showNumber(self):
+        print(self.real,"i +",self.img,"j")
+
+    def __add__(self,num2):
+        newreal = self.real + num2.real
+        newimg = self.img + num2.img
+        return Complex(newreal,newimg)
+    
+
+num1=Complex(1,2)
+num1.showNumber()
+
+num2=Complex(5,2)
+num2.showNumber()
+
+num3 = num1+num2
+num3.showNumber()
